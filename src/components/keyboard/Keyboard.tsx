cc2ec163e3d95b4ced18364d7e95653a44bf1947
Denis <chentsov@restream.rt.ru>
2022-02-16 12:03:18 +0300

    Add putin

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -52,7 +52,7 @@ export const Keyboard = ({
   return (
     <div>
       <div className="flex justify-center mb-1">
-        {['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'].map((key) => (
+        {['П', 'У', 'т', 'И', 'Н', 'П', 'У', 'Т', 'И', 'Н'].map((key) => (
           <Key
             value={key}
             key={key}
@@ -63,7 +63,7 @@ export const Keyboard = ({
         ))}
       </div>
       <div className="flex justify-center mb-1">
-        {['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'].map((key) => (
+        {['П', 'У', 'т', 'И', 'Н', 'П', 'У', 'Т', 'И'].map((key) => (
           <Key
             value={key}
             key={key}
@@ -77,7 +77,7 @@ export const Keyboard = ({
         <Key width={65.4} value="ENTER" onClick={onClick}>
           {ENTER_TEXT}
         </Key>
-        {['Z', 'X', 'C', 'V', 'B', 'N', 'M'].map((key) => (
+        {['П', 'У', 'т', 'И', 'Н', 'П', 'У'].map((key) => (
           <Key
             value={key}
             key={key}
